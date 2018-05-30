13099947db676f8552bb98e84e805ef73391e70e
Justin Liu <justin_liu@hotmail.co.uk>
2018-05-30 13:48:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.getElementById("nested").querySelector(".target")
+}
+
+function increaseRankBy(n){
+  var rank = document.querySelectorAll(".ranked-list");
+  for (var i = 0; i < rank.length; i++){
+    rank[i].innerHTML = (parseInt(rank[i])+n);
+  }
+  return rank;
+}
+
+function deepestChild(){
+  return document.querySelector("#grand-node").querySelector("div div div div div")
+}
+
+
+
+
+
