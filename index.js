ba6d8caf07d23f07c41309d177a9c9c6c14117e6
Justin Liu <justin_liu@hotmail.co.uk>
2018-05-30 13:46:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  return document.querySelector("#grand-node").querySelector("div div div")
+  return document.querySelector("#grand-node").querySelector("div div div div")
 }
 
 
